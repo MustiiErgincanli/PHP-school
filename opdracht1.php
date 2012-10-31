99363d0e2a87fe63302913a2b2fcf8ed843951a8
Nathan Sakoetoe <info@zavox.nl>
2012-10-31 12:11:23 +0100

    Update opdracht1.php

--- opdracht1.php
+++ opdracht1.php
@@ -9,12 +9,12 @@
 	$som = 0;
 	
 	//Met een loopje herhaal je iets
-	for ($i = 0; $i < 1000; $i++) {
+	for ($i = 0; $i < 1000; $i++) 
 			//Met is_int checken of het om een heel getal gaat.
-			if (is_int($i / 3) || is_int($i / 5)) {
+			if (is_int($i / 3) || is_int($i / 5)) 
 				$som = $som + $i;			
-    }
-}
+    
+
 	//Uitkomst van de som wordt weergeven.
 	echo $som;
 	
